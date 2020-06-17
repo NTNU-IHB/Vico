fdf4ae46ac0fc5c00584a6400af5ebb1b4b80a72
Lars Ivar Hatledal <laht@hials.no>
2020-06-17 10:12:57 +0200

    update

--- core/src/main/kotlin/no/ntnu/ihb/acco/core/Component.kt
+++ core/src/main/kotlin/no/ntnu/ihb/acco/core/Component.kt
@@ -10,5 +10,9 @@ interface CoSimulationComponent : Component {
     val variables: Map<String, Var<*>>
 
     fun getVariable(name: String) = variables[name]
+    fun getIntegerVariable(name: String) = variables[name] as IntVar
+    fun getRealVariable(name: String) = variables[name] as RealVar
+    fun getStringVariable(name: String) = variables[name] as StrVar
+    fun getBooleanVariable(name: String) = variables[name] as BoolVar
 
 }
