bf6459ac3e18eb2f29879dfe944d504652b41438
Lars Ivar Hatledal <laht@hials.no>
2020-08-26 14:06:23 +0200

    Add visualisation

--- examples/quarter-truck/dsl/QuarterTruck.main.kts
+++ examples/quarter-truck/dsl/QuarterTruck.main.kts
@@ -4,15 +4,25 @@
 @file:DependsOn("no.ntnu.ihb.vico:dsl:0.1.0")
 @file:DependsOn("no.ntnu.ihb.vico:fmi:0.1.0")
 @file:DependsOn("no.ntnu.ihb.vico:chart:0.1.0")
+@file:DependsOn("no.ntnu.ihb.vico:jme-render:0.1.0")
 
 import no.ntnu.ihb.vico.SlaveComponent
 import no.ntnu.ihb.vico.SlaveSystem
 import no.ntnu.ihb.vico.chart.TimeSeriesDrawer
+import no.ntnu.ihb.vico.components.PositionRefComponent
+import no.ntnu.ihb.vico.components.TransformComponent
 import no.ntnu.ihb.vico.dsl.execution
 import no.ntnu.ihb.vico.log.LogConfigBuilder
 import no.ntnu.ihb.vico.log.SlaveLoggerSystem
 import no.ntnu.ihb.vico.master.FixedStepMaster
+import no.ntnu.ihb.vico.math.Vector3d_X
+import no.ntnu.ihb.vico.render.GeometryComponent
+import no.ntnu.ihb.vico.render.jme.JmeRenderSystem
+import no.ntnu.ihb.vico.shapes.BoxShape
+import no.ntnu.ihb.vico.shapes.PlaneShape
 import no.ntnu.ihb.vico.structure.RealParameter
+import no.ntnu.ihb.vico.systems.PositionRefSystem
+import org.joml.Vector3f
 
 execution {
 
@@ -22,6 +32,10 @@ execution {
 
         entity("ground") {
             component(SlaveComponent("ground.fmu", "ground"))
+            component(TransformComponent())
+            component(GeometryComponent(PlaneShape(5f, 2f)).apply {
+                offsetTransform.rotate(-Math.PI / 2, Vector3d_X, offsetTransform)
+            })
         }
 
         entity("chassis") {
@@ -33,6 +47,12 @@ execution {
                     RealParameter("R.dChassis", 1000.0),
                 )
             })
+            component(TransformComponent())
+            component(GeometryComponent(BoxShape(Vector3f(0.75f, 0.25f, 0.75f))).setColor(0x0000ff))
+
+            component {
+                PositionRefComponent(yRef = "zChassis")
+            }
         }
 
         entity("wheel") {
@@ -44,6 +64,12 @@ execution {
                     RealParameter("R.dWheel", 0.0),
                 )
             })
+            component(TransformComponent())
+            component(GeometryComponent(BoxShape(Vector3f(1f, 0.25f, 1f))).setColor(0x0000ff))
+
+            component {
+                PositionRefComponent(yRef = "zWheel")
+            }
         }
 
     }
@@ -66,6 +92,12 @@ execution {
                 .isLive(true)
                 .build()
         }
+        system {
+            PositionRefSystem()
+        }
+        system {
+            JmeRenderSystem()
+        }
 
     }
 
@@ -79,5 +111,6 @@ execution {
     }
 
 }.runner.apply {
+    targetRealTimeFactor = 0.5
     startAndWait()
 }
