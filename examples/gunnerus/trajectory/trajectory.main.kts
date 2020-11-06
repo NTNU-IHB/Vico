2899c6e7dc2dadde5dfb91fb58bb53c2f33ed636
Lars Ivar Hatledal <laht@ntnu.no>
2020-11-06 15:12:27 +0100

    update sppgen

--- examples/gunnerus/trajectory/trajectory.main.kts
+++ examples/gunnerus/trajectory/trajectory.main.kts
@@ -1,6 +1,6 @@
 @file:Repository("https://dl.bintray.com/ntnu-ihb/mvn")
 
-@file:DependsOn("no.ntnu.ihb.sspgen:dsl:0.3.1")
+@file:DependsOn("no.ntnu.ihb.sspgen:dsl:0.3.2")
 
 import no.ntnu.ihb.sspgen.dsl.ssp
 import kotlin.math.PI
