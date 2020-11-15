693c610d1f57f9f1a7063820b61804f8ce45dfd7
Lars Ivar Hatledal <laht@hials.no>
2020-11-15 20:31:38 +0100

    update demo

--- examples/gunnerus/trajectory/run.sh
+++ examples/gunnerus/trajectory/run.sh
@@ -1,1 +1,1 @@
-vico simulate-ssp -dt "0.05" --stopTime "2000" -log "LogConfig.xml" -visual "VisualConfig.xml" --scenario "scenario.main.kts" -p "initialValues" -res "results" "gunnerus-trajectory.ssp"
+vico simulate-ssp -dt "0.05" --stopTime "2000" -log "LogConfig.xml" -visual "VisualConfig.xml" -chart "ChartConfig.xml" --scenario "scenario.main.kts" -p "initialValues" -res "results" "gunnerus-trajectory.ssp"
