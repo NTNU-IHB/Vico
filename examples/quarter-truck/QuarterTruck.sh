388a93f2a057029c5d4f8c2bd75e07eebee779b4
Lars Ivar Hatledal <laht@ntnu.no>
2020-12-08 14:32:53 +0100

    Update examples

--- examples/quarter-truck/QuarterTruck.sh
+++ examples/quarter-truck/QuarterTruck.sh
@@ -1,4 +1,4 @@
-vico simulate-ssp -dt "1e-2" --stopTime "10" -log "LogConfig.xml" -chart "ChartConfig.xml" -p "initialValues" -res "results" "QuarterTruck2.ssp"
+vico simulate-ssp -dt "1e-2" --stopTime "10" -log "LogConfig.xml" -chart "ChartConfig.xml" -p "initialValues" -res "results" "QuarterTruck.ssp"
 
 
 
