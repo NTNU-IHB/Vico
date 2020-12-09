42de39fc2894c5fc1f7c9e2f7d5a93b38635b3a1
Lars Ivar Hatledal <laht@ntnu.no>
2020-12-09 13:56:16 +0100

    Update

--- examples/SimpleTest.main.kts
+++ examples/SimpleTest.main.kts
@@ -1,7 +1,10 @@
+#!kts
+
+@file:Repository("mavenLocal")
 @file:Repository("https://dl.bintray.com/laht/mvn")
 
-@file:DependsOn("no.ntnu.ihb.vico:core:0.2.0")
-@file:DependsOn("no.ntnu.ihb.vico:threekt-render:0.2.0")
+@file:DependsOn("no.ntnu.ihb.vico:core:0.3.0", options = arrayOf("scope=compile,runtime"))
+@file:DependsOn("no.ntnu.ihb.vico:threekt-render:0.3.0", options = arrayOf("scope=compile,runtime"))
 
 @file:CompilerOptions("-jvm-target", "1.8")
 
@@ -13,12 +16,11 @@ import no.ntnu.ihb.vico.render.GeometryRenderer
 import no.ntnu.ihb.vico.render.mesh.BoxMesh
 
 val renderer = ThreektRenderer()
-
 EngineBuilder().renderer(renderer).build().use { engine ->
 
     val e1 = engine.createEntity("e1")
-    e1.addComponent<Transform>()
-    e1.addComponent(Geometry(BoxMesh()))
+    e1.add<Transform>()
+    e1.add(Geometry(BoxMesh()))
 
     engine.addSystem(GeometryRenderer())
 
