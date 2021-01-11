f6f8ce524280c4e7942a5d46a48e53cb4688834c
Lars Ivar Hatledal <laht@ntnu.no>
2021-01-11 10:53:03 +0100

    wip

--- fmi/src/main/kotlin/no/ntnu/ihb/vico/master/FixedStepMaster.kt
+++ fmi/src/main/kotlin/no/ntnu/ihb/vico/master/FixedStepMaster.kt
@@ -1,13 +1,20 @@
 package no.ntnu.ihb.vico.master
 
+import no.ntnu.ihb.vico.FmiSlave
 import no.ntnu.ihb.vico.SlaveInitCallback
 import no.ntnu.ihb.vico.SlaveStepCallback
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.math.ceil
+import kotlin.math.max
 
 class FixedStepMaster @JvmOverloads constructor(
-        private val parallel: Boolean = true
+    private val fixedStepSize: Double,
+    private val parallel: Boolean = true
 ) : MasterAlgorithm() {
+
+    private val stepNumber: Long = 0
+    private val decimationMap: MutableMap<FmiSlave, Long> = mutableMapOf()
 
     override fun initialize(currentTime: Double, slaveInitCallback: SlaveInitCallback) {
         slaves.forEach { slave ->
@@ -23,6 +30,10 @@ class FixedStepMaster @JvmOverloads constructor(
             slave.exitInitializationMode()
         }
         readAllVariables()
+
+        slaves.forEach { slave ->
+            decimationMap[slave] = calculateStepFactor(slave, fixedStepSize)
+        }
     }
 
     override fun step(
@@ -33,10 +44,13 @@ class FixedStepMaster @JvmOverloads constructor(
         val tNext = currentTime + stepSize
         val stream = if (parallel) slaves.parallelStream() else slaves.stream()
         stream.forEach { slave ->
-            slave.transferCachedSets()
-            slave.doStep(stepSize)
-            slave.retrieveCachedGets()
-            slaveStepCallback.invoke(tNext to slave.component)
+            val decimationFactor = decimationMap[slave] ?: 1L
+            if (decimationFactor % stepNumber == 0L) {
+                slave.transferCachedSets()
+                slave.doStep(stepSize)
+                slave.retrieveCachedGets()
+                slaveStepCallback.invoke(tNext to slave.component)
+            }
         }
     }
 
@@ -44,15 +58,15 @@ class FixedStepMaster @JvmOverloads constructor(
 
         private val LOG: Logger = LoggerFactory.getLogger(MasterAlgorithm::class.java)
 
-        /* fun calculateStepFactor(slave: SlaveComponent, baseStepSize: Double): Long {
-             val stepSizeHint: Double = slave.stepSizeHint ?: return 1
-             val decimationFactor = max(1, ceil(stepSizeHint / baseStepSize).toLong())
-             val actualStepSize = baseStepSize * decimationFactor
-             if (actualStepSize.compareTo(stepSizeHint) != 0) {
-                 LOG.warn("Actual step size for ${slave.instanceName} will be $actualStepSize rather than requested value $stepSizeHint.")
-             }
-             return decimationFactor
-         }*/
+        fun calculateStepFactor(slave: FmiSlave, baseStepSize: Double): Long {
+            val stepSizeHint: Double = slave.component.stepSizeHint ?: return 1
+            val decimationFactor = max(1, ceil(stepSizeHint / baseStepSize).toLong())
+            val actualStepSize = baseStepSize * decimationFactor
+            if (actualStepSize.compareTo(stepSizeHint) != 0) {
+                LOG.warn("Actual step size for ${slave.instanceName} will be $actualStepSize rather than requested value $stepSizeHint.")
+            }
+            return decimationFactor
+        }
 
     }
 
