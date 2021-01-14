49e19ba614c85721d25a1bc7c6c805b906d00815
Lars Ivar Hatledal <laht@hials.no>
2021-01-14 09:34:00 +0100

    refactor run.sh

--- examples/gunnerus/trajectory/run.sh
+++ examples/gunnerus/trajectory/run.sh
@@ -1,2 +1,10 @@
-vico simulate-ssp -dt "0.05" --stopTime "2000" -log "LogConfig.xml" -visual "VisualConfig.xml" --scenario "scenario.main.kts" -p "initialValues" -res "results" "gunnerus-trajectory.ssp"
+vico simulate-ssp \
+-dt "0.05" \
+--stopTime "2000" \
+-log "LogConfig.xml" \
+-visual "VisualConfig.xml" \
+-s "scenario.main.kts" \
+-p "initialValues" \
+-res "results" \
+"gunnerus-trajectory.ssp"
 read -p "Press enter to continue"
