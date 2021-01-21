21685d21443b3661a95e578377b4914e87165f6e
Lars Ivar Hatledal <laht@ntnu.no>
2021-01-21 12:52:00 +0100

    Add clamping modifier

--- core/src/main/kotlin/no/ntnu/ihb/vico/core/modifer.kt
+++ core/src/main/kotlin/no/ntnu/ihb/vico/core/modifer.kt
@@ -1,8 +1,21 @@
 package no.ntnu.ihb.vico.core
 
+import no.ntnu.ihb.vico.math.clamp
+
 fun interface RealModifier {
 
     fun invoke(originalValue: Double): Double
+
+}
+
+data class ClampModifier(
+    val min: Double,
+    val max: Double
+) : RealModifier {
+
+    override fun invoke(originalValue: Double): Double {
+        return clamp(originalValue, min, max)
+    }
 
 }
 
