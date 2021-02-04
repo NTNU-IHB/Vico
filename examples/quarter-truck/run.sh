efcb47c80dff0ff36971a1b9bc085985b00cea56
Lars Ivar Hatledal <laht@hials.no>
2021-02-04 12:00:45 +0100

    refactor

--- examples/quarter-truck/run.sh
+++ examples/quarter-truck/run.sh
@@ -1,2 +1,9 @@
-vico simulate-ssp -dt "1e-2" --stopTime "10" -log "LogConfig.xml" -chart "ChartConfig.xml" -p "initialValues" -res "results" "QuarterTruck.ssp"
+vico simulate-ssp \
+-dt "1e-2" \
+--stopTime "10" \
+-log "LogConfig.xml" \
+-chart "ChartConfig.xml" \
+-p "initialValues" \
+-res "results" \
+"QuarterTruck.ssp"
 read -p "Press enter to continue"
