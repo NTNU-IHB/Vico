255ddf32703871941d8663f84dbffc81363dcbff
Lars Ivar Hatledal <laht@ntnu.no>
2021-03-05 14:50:14 +0100

    wip

--- web/src/test/kotlin/no/ntnu/ihb/vico/web/mesh.kt
+++ web/src/test/kotlin/no/ntnu/ihb/vico/web/mesh.kt
@@ -3,7 +3,9 @@ package no.ntnu.ihb.vico.web
 import no.ntnu.ihb.vico.KtorServer
 import no.ntnu.ihb.vico.components.Transform
 import no.ntnu.ihb.vico.core.EngineBuilder
+import no.ntnu.ihb.vico.render.Camera
 import no.ntnu.ihb.vico.render.Geometry
+import no.ntnu.ihb.vico.render.Water
 import no.ntnu.ihb.vico.render.loaders.ObjLoader
 import java.io.File
 import kotlin.system.exitProcess
@@ -13,12 +15,17 @@ fun main() {
 
     EngineBuilder().build().use { engine ->
 
-        val e1 = engine.createEntity("e1")
-        e1.add<Transform>().apply {
-            setLocalTranslation(-5.0, 0.0, 0.0)
+        engine.createEntity("e1").apply {
+            val mesh = ObjLoader().load(File("examples/gunnerus/obj/Gunnerus.obj"))
+            add(Geometry(mesh))
         }
-        val mesh = ObjLoader().load(File("examples/gunnerus/obj/Gunnerus.obj"))
-        e1.add(Geometry(mesh))
+
+        engine.createEntity("water", Water(500)).apply {
+            add<Transform>().localTranslateY(-1.0)
+        }
+        engine.createEntity("camera", Camera()).apply {
+            add<Transform>().setLocalTranslation(-50.0, 50.0, -75.0)
+        }
 
         engine.addSystem(SineMoverSystem())
         engine.addSystem(KtorServer(8000))
