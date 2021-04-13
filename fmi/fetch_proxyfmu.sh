1cd1b8d8e8c52e6cef5dce80602be62b42e12b67
Lars Ivar Hatledal <laht@hials.no>
2021-04-13 19:58:20 +0200

    test

--- fmi/fetch_proxyfmu.sh
+++ fmi/fetch_proxyfmu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 conan remote add osp https://osp.jfrog.io/artifactory/api/conan/conan-local -f
-conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=build/cmake-build-release --build=missing;;
+conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=build/cmake-build-release --build=missing
 
