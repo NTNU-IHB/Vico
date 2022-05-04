75a3823e2c96ade1bf9a51e1d110ac96ef4f1473
Lars Ivar Hatledal <laht@ntnu.no>
2022-05-04 08:49:11 +0200

    re-add eval using main.kts

--- cli/src/main/kotlin/no/ntnu/ihb/vico/cli/VicoCLI.kt
+++ cli/src/main/kotlin/no/ntnu/ihb/vico/cli/VicoCLI.kt
@@ -1,12 +1,13 @@
 package no.ntnu.ihb.vico.cli
 
 import no.ntnu.ihb.vico.Vico
+import no.ntnu.ihb.vico.cli.commands.Eval
 import no.ntnu.ihb.vico.cli.commands.SimulateFmu
 import no.ntnu.ihb.vico.cli.commands.SimulateSsp
 import picocli.CommandLine
 
 @CommandLine.Command(
-    subcommands = [SimulateFmu::class, SimulateSsp::class]
+    subcommands = [SimulateFmu::class, SimulateSsp::class, Eval::class]
 )
 class VicoCLI : Runnable {
 
